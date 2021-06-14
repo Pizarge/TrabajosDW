2592c5e75099b572ac09b015162201488948ef63
joenpiuc <jpizarrouceda@gmail.com>
2021-06-14 13:00:06 -0500

    sesiontodoa

--- Sesion18/js/agendav3.js
+++ Sesion18/js/agendav3.js
@@ -17,31 +17,47 @@ function guardarDato() {
     //actualizando la lista
     actualizarDatos();
 }
+
 function recuperarDato(){
-    var nombre = document.getElementById("nombre").value;
-    localStorage.getItem(nombre);
-    document.getElementById("movil").value = localStorage.getItem(nombre);
-    document.getElementById("email").value = localStorage.getItem(nombre);
+    // Capturando el nombre
+    const nombre = document.getElementById("nombre").value;
+    // Parseando los datos
+    let datos = localStorage.getItem(nombre);
+    datos = JSON.parse(datos);
+    // Incluyendo los datos en el HTML
+    document.getElementById("movil").value = datos.movil;
+    document.getElementById("email").value = datos.email;
 }
+
 function eliminarDato(){
-    var nombre = document.getElementById("nombre").value;
+    // Capturando el nombre
+    const nombre = document.getElementById("nombre").value;
+    // Borrando el dato
     localStorage.removeItem(nombre);
+    // Actualizando la lista
     actualizarDatos();
 }
+
 function eliminarTodo(){
+    // Borrando todos los datos
     localStorage.clear();
+    // Actualizando la lista
     actualizarDatos();
 }
+
 function actualizarDatos(){
-    var registro = JSON.parse(localStorage.getItem("registro"));
-    if (localStorage.length === 0){
-        registro += '<li>Vacío</li>'
-    }else{
-        for(var i=0; i <= localStorage.length - 1; i++){
-            var key =localStorage.key(i);
-            registro += '<li>' + '<span class="nom">' + key + '</span>' 
-            + '<span class="nom">' + localStorage.getItem(key) + '</span>' + '</li><br>';
+    var registro = "";
+    if (localStorage.length === 0) {
+        registro += '<li>Vacío</li>';
+    } else {
+        for (var i = 0; i <= localStorage.length - 1; i++) {
+            const key = localStorage.key(i);
+            // Parseando los datos 
+            let datos = localStorage.getItem(key);
+            datos = JSON.parse(datos);
+
+            registro += `<li> <span class="nom">${key} </span> <span class="nom"> ${datos.movil} </span> <span class="nomemail"> ${datos.email} </span></li><br>`;
         }
     }
-    document.getElementById('contactos').innerHTML = registro;
+    document.getElementById("contactos").innerHTML = registro;
 }
