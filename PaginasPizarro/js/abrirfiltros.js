74980ee6ce4e96e9c50af62fbfb1f7649cedc4ee
joenpiuc <jpizarrouceda@gmail.com>
2021-07-10 02:37:02 -0500

    TODOA

---
+++ PaginasPizarro/js/abrirfiltros.js
@@ -0,0 +1,31 @@
+//ocultar o mostrar una caja
+$("#c1b").click(function(){
+        $(".category_list").toggle(1500);
+    }, function() {
+        $(".category_list").toggle(1500);
+})
+
+
+$("#c2b").click(function(){
+    $(".checkbox_list").toggle(1500);
+}, function() {
+    $(".checkbox_list").toggle(1500);
+})
+
+$("#cPb").click(function(){
+    $(".checkbox_list_cPb").toggle(1500);
+}, function() {
+    $(".checkbox_list_cPb").toggle(1500);
+})
+
+$("#c3b").click(function(){
+    $(".checkbox_list_c3b").toggle(1500);
+}, function() {
+    $(".checkbox_list_c3b").toggle(1500);
+})
+
+$("#c4b").click(function(){
+    $(".checkbox_list_c4b").toggle(1500);
+}, function() {
+    $(".checkbox_list_c4b").toggle(1500);
+})
