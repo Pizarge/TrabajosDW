0a6a285599e8f67005c8cad4c96aa2ab1962e25f
joenpiuc <jpizarrouceda@gmail.com>
2021-07-09 22:25:55 -0500

    todoa

---
+++ PROYECTO FINAL (paginas Pizarro)/TECNOLOGIA/js/abrirfiltros.js
@@ -0,0 +1,25 @@
+//ocultar o mostrar una caja
+$("#c1b").click(function(){
+        $(".category_list").toggle(1500);
+    }, function() {
+        $(".category_list").toggle(1500);
+})
+
+
+$("#c2b").click(function(){
+    $(".Price_bar").toggle(1500);
+}, function() {
+    $(".Price_bar").toggle(1500);
+})
+
+$("#c3b").click(function(){
+    $(".checkbox_list_c3b").toggle(1500);
+}, function() {
+    $(".checkbox_list_c3b").toggle(1500);
+})
+
+$("#c4b").click(function(){
+    $(".checkbox_list_c4b").toggle(1500);
+}, function() {
+    $(".checkbox_list_c4b").toggle(1500);
+})
---
+++ PROYECTO FINAL/js/abrirfiltros.js
@@ -0,0 +1,13 @@
+//ocultar o mostrar una caja
+$("#c1b").click(function(){
+        $(".category_list").toggle(1500);
+    }, function() {
+        $(".category_list").toggle(1500);
+})
+
+
+$("#c2b").click(function(){
+    $(".checkbox_list").toggle(1500);
+}, function() {
+    $(".checkbox_list").toggle(1500);
+})
